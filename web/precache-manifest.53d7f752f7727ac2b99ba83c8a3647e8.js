45c08c5292d5235fe61a97e77281939756577738
João Victor Nascimento <victorjoao97@outlook.com>
2020-04-26 22:24:59 -0300

    add web

---
+++ web/precache-manifest.53d7f752f7727ac2b99ba83c8a3647e8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451ee51fb7a68d19880cad910068c7e9",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "4e553118b2f575b00cca",
+    "url": "/web/static/css/2.24df1876.chunk.css"
+  },
+  {
+    "revision": "07f6871dec2019bf3f4e",
+    "url": "/web/static/css/main.35dc738f.chunk.css"
+  },
+  {
+    "revision": "4e553118b2f575b00cca",
+    "url": "/web/static/js/2.787e38ef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/web/static/js/2.787e38ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f6871dec2019bf3f4e",
+    "url": "/web/static/js/main.fbe5fc6b.chunk.js"
+  },
+  {
+    "revision": "5260d7bec9a08f430bb8",
+    "url": "/web/static/js/runtime-main.f61e9bf7.js"
+  },
+  {
+    "revision": "51adb19f1b35d782871a57aa387c9930",
+    "url": "/web/static/media/bg.51adb19f.jpg"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/web/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "/web/static/media/logo.7eea718b.svg"
+  },
+  {
+    "revision": "b2dbe1abefe10b7f99138879128fd14c",
+    "url": "/web/static/media/logo.b2dbe1ab.png"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/web/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/web/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/web/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/web/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/web/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/web/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/web/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/web/static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
